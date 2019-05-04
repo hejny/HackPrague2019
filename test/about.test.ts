4018555f8bc1261f359b04da3e4b2c8e754a8a22
Pavol Hejny <me@pavolhejny.com>
2019-05-04 16:35:39 +0200

    About test coverage

--- test/about.test.ts
+++ test/about.test.ts
@@ -1,5 +1,8 @@
 import * as request from 'supertest';
 import { createApp } from '../src/createApp';
+import * as path from 'path';
+
+const { version } = require(path.join(__dirname, '../package.json'));
 
 const app = createApp();
 
@@ -7,5 +10,10 @@ export default describe('About route', () => {
     it('should get version number', () =>
         request(app)
             .get(`/about`)
-            .expect(200));
+            .expect(200)
+            .expect({ version }));
+    /*.expect((res) => {
+                console.log('res', res.body);
+                res.body.should.include('version');
+            })*/
 });
