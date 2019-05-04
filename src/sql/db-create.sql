3f8588b8bb327ce37a66d07c8bdefaa4565070c0
Pavol Hejny <me@pavolhejny.com>
2019-05-05 01:13:24 +0200

    SQL migrations

--- src/sql/db-create.sql
+++ src/sql/db-create.sql
@@ -1,2 +1,23 @@
-
 SET NAMES utf8;
+
+DROP TABLE IF EXISTS `Item`;
+CREATE TABLE `Item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(36) COLLATE utf8_bin NOT NULL,
+  `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+DROP TABLE IF EXISTS `Log`;
+CREATE TABLE `Log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `endpoint` varchar(1000) COLLATE utf8_bin NOT NULL,
+  `apiKey` varchar(1000) COLLATE utf8_bin NOT NULL,
+  `statusCode` char(3) COLLATE utf8_bin NOT NULL,
+  `request` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `response` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
